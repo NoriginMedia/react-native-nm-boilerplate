0f355a9eaf38036f170a90a6d54d3870277e4912
asgvard <asgvard@gmail.com>
2017-01-30 15:19:00 +0100

    ESLint fix

--- src/js/web/components/LiveChannel.js
+++ src/js/web/components/LiveChannel.js
@@ -145,7 +145,6 @@ class LiveChannel extends React.Component {
 }
 
 LiveChannel.propTypes = {
-	title: PropTypes.string.isRequired,
 	images: PropTypes.shape({
 		LOGO: PropTypes.string
 	}).isRequired,
